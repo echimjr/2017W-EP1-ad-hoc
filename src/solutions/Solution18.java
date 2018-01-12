d1e072867fd1deba96d8e8d1fbb9de8e9fef8fe3
rob <->
2018-01-12 18:14:57 +0100

    added tasks + solutions for 3rd K1 AdHoc (Fr12a)

---
+++ src/solutions/Solution18.java
@@ -0,0 +1,20 @@
+/**
+ * Task 18) Löse rekursiv: Zeichne auf einen 600x600 Canvas vertikale Blöcke, der erste Block geht von ganz unten bis
+ * nach oben. Nachfolgende Blöcke sollen 15% kleiner sein als die vorhergehenden, der letzte Block soll rechts
+ * am Bildschirmrand enden
+ */
+
+public class Solution18 {
+    public static void rec(int n, int val, double height, double drawX) {
+        if (val > n) return;
+        double width = 600./((double)n);
+        StdDraw.filledRectangle(drawX, 300, width, height/2);
+        rec(n, val+1, height * 0.85, drawX + width);
+    }
+
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(600,600);
+        StdDraw.setScale(0,600);
+        rec(100,1, 600,0);
+    }
+}
